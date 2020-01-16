e5c2a60b30758fe6dde72d106ce7f96ca5128f06
AdileneRobles <58531700+AdileneRobles@users.noreply.github.com>
2020-01-16 11:51:09 -0700

    changes advanced Node.js

---
+++ ADVANCED NODE.JS/4.Node for Networking/4.DNS Module/dns.js
@@ -0,0 +1,21 @@
+const dns = require('dns'); // name -- addresses
+
+/*dns.lookup('pluralsight.com', (err, address) => {
+  console.log(address);
+});*/
+
+/*dns.resolve4('pluralsight.com', (err, address) => {
+   console.log(address);
+});*/
+
+/*dns.resolve4('pluralsight.com', 'MS', (err, address) => {
+    console.log(address);
+});*/
+
+/*dns.resolveMx('pluralsight.com', (err, address) => {
+  console.log(address);
+});*/
+
+dns.reverse('35.161.75.227', (err, hostnames) => {
+  console.log(hostnames);
+});
